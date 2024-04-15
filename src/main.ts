cc6488ea29c8dbe35fe8c62d98d16ebe735ee827
Willian Marques <willian.goncalves4@gmail.com>
2024-04-15 16:08:05 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify';
+
+class Application {
+    async start() {
+        const server = fastify();
+
+        server.get('/ping', (_req, _reply) => {
+            return 'pong';
+        });
+
+        await server.listen({
+            port: 3333,
+        });
+
+        console.log('server started 👌');
+    }
+}
+
+new Application().start();
